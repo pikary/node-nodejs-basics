0310edab1a1411d860ebae4c73c788210f52f349
nurlan <baitassovnurlan1507@gmail.com>
2024-10-07 23:49:14 +0500

    fix: moved new Transform() into function transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,13 +1,15 @@
-import { Transform } from 'stream';
+import {Transform} from 'stream';
 
-// Создаем класс для преобразования текста (инверсии)
-const reverseText = new Transform({
-    transform(chunk, encoding, callback) {
-        // Преобразуем данные в строку, разворачиваем и передаем дальше
-        const reversedChunk = chunk.toString().split('').reverse().join('');
-        callback(null, reversedChunk);
-    }
-});
+const transform = async () => {
+    process.stdout.write('\nInput your text:\n');
+    const transformer = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversedValue = chunk.toString().split('').reverse().join('');
+            callback(null, reversedValue);
+        }
+    })
+    process.stdin.pipe(transformer).pipe(process.stdout);
 
-// Связываем потоки: process.stdin -> reverseText -> process.stdout
-process.stdin.pipe(reverseText).pipe(process.stdout);
+};
+
+await transform();
