047a7e161053b7edb9178fee3caa59a75ef447f7
nurlan <baitassovnurlan1507@gmail.com>
2024-10-08 03:12:15 +0500

    refactor: use import.meta.dirname instead of fileUrltoPath, removes deleting file after compressing

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -3,10 +3,7 @@ import path from "path";
 import zlib from "zlib";
 import {promisify} from "util";
 import {pipeline} from "stream";
-import {fileURLToPath} from "url";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 const asyncPipeline = promisify(pipeline)
 const asyncDeleteZip = promisify(fs.unlink)
@@ -14,11 +11,11 @@ const asyncDeleteZip = promisify(fs.unlink)
 const decompress = async () => {
     try{
         await asyncPipeline(
-            fs.createReadStream(path.join(__dirname,'files','archive.gz')),
+            fs.createReadStream(path.join(import.meta.dirname,'files','archive.gz')),
             zlib.createUnzip(),
-            fs.createWriteStream(path.join(__dirname,'files','fileToCompress.txt'))
+            fs.createWriteStream(path.join(import.meta.dirname,'files','fileToCompress.txt'))
         )
-        await asyncDeleteZip(path.join(__dirname,'files','archive.gz'))
+        // await asyncDeleteZip(path.join(import.meta.dirname,'files','archive.gz'))
     }catch (e){
         console.error(e)
     }
