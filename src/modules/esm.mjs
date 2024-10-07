6eecf5a00f6835bff7ad2022ded4bf742166c7f7
nurlan <baitassovnurlan1507@gmail.com>
2024-10-08 00:37:24 +0500

    refactor: use import.meta instead of fileToUrl

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -2,7 +2,6 @@ import path from "path";
 import {release,version} from 'os'
 import {createServer as createServerHttp, createServer} from 'http'
 import './files/c.js'
-import {fileURLToPath} from 'url'
 
 const random = Math.random();
 
@@ -13,19 +12,18 @@ if (random > 0.5) {
     const { default: data } = await import('./files/a.json', { with: { type: 'json' } });
     unknownObject = data
 } else {
-    const { default: data } = await import('./files/a.json', { with: { type: 'json' } });
+    const { default: data } = await import('./files/a.json', { with: { type: "json" } });
     unknownObject = data
 }
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename);
+
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
+console.log(`Path to current file is ${import.meta.filename}`);
+console.log(`Path to current directory is ${import.meta.dirname}`);
 
 const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
--- src/streams/read.js
+++ src/streams/read.js
@@ -3,11 +3,28 @@ import path from "path";
 
 
 const read = async () => {
-    // Write your code here
-    const readStream = fs.createReadStream(path.join(import.meta.dirname, 'files','fileToRead.txt'), {encoding:'utf-8'});
-    readStream.on('data', (chunk) => {
-        process.stdout.write(chunk.toString());
-    })
+    try {
+        const filePath = path.join(import.meta.dirname, 'files', 'fileToRead.txt');
+        const readStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+
+        readStream.on('readable', () => {
+            let chunk
+            while (null !== (chunk = readStream.read())) {
+                process.stdout.write(chunk);
+            }
+        });
+
+        readStream.on('error', (err) => {
+            throw err
+        })
+
+        readStream.on('end', () => {
+            console.log('\nFile read completed');
+        });
+
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 await read();
